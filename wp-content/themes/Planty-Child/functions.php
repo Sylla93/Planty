d8dc83b12e00d85e53dab570599c6d9ebceeff31
Sylla <syllasiddy11@gmail.com>
2024-07-24 21:12:00 +0200

    Ajout des images sur mon projet git

--- wp-content/themes/Planty-Child/functions.php
+++ wp-content/themes/Planty-Child/functions.php
@@ -4,8 +4,8 @@ add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/assets/theme.css', array(), 
-    filemtime(get_stylesheet_directory() . '/assets/theme.css'));
+    wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/CSS/theme.css', array(), 
+    filemtime(get_stylesheet_directory() . '/CSS/theme.css'));
 }
 
 function planty_canette() {
