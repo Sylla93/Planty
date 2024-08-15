0ca186c0e8817a373cb34a7407eb25b9cad54495
Sylla <syllasiddy11@gmail.com>
2024-08-15 13:57:09 +0200

    FIN des de la page commander,responsive de toutes les pages et la partie admin fait aussi avec git

--- wp-content/themes/Planty-Child/functions.php
+++ wp-content/themes/Planty-Child/functions.php
@@ -8,6 +8,45 @@ function theme_enqueue_styles()
     filemtime(get_stylesheet_directory() . '/CSS/theme.css'));
 }
 
+function add_items_menu($items, $args) {
+    if ($args->theme_location == 'primary') {
+        // Convertit les éléments du menu en tableau
+        $menu_items = explode('</li>', $items);
+        // Lien Admin à ajouter
+        $admin_link = '<li class="centrerAdmin"><a href="' . admin_url() . '" class="adm">Admin</a></li>';
+        // Position où insérer le lien (après le deuxième élément)
+        $position = 1;
+        // Insère le lien Admin à la position spécifiée
+        array_splice($menu_items, $position, 0, $admin_link);
+        // Reconstruit le menu à partir du tableau modifié
+        $items = implode('</li>', $menu_items);
+        // Ajoute le tag de fermeture '</li>' au dernier élément
+        if (substr($items, -5) !== '</li>') {
+            $items .= '</li>';
+        }
+    }
+    return $items;
+}
+
+add_filter('wp_nav_menu_items', 'add_items_menu', 10, 2);
+
+
+// if (is_user_logged_in() && $args->theme_location == 'primary') {
+       
+//     $menu_items = explode('</li>', $items);
+    
+//     $admin_link = '<li><a href="' . admin_url() . '">Admin</a></li>';
+  
+//     $position = 2;
+   
+//     array_splice($menu_items, $position, 0, $admin_link);
+    
+//     $items = implode('</li>', $menu_items);
+// }
+// return $items;
+
+
+
 function planty_canette() {
     ob_start(); ?>
         <div class="container">
@@ -20,3 +59,15 @@ function planty_canette() {
     <?php return ob_get_clean();
 }
 add_shortcode('planty_display_canette', 'planty_canette');
+
+// ***************************************************************************************************
+
+function planty_fraise() {
+    ob_start(); ?>
+    <div class= "case_fraise">
+        <input <?php echo get_stylesheet_directory_uri(); ?> type="number" class="centrer" name="montant" min="0" step="0.01" required>
+    </div>
+    <?php return ob_get_clean();
+
+}
+add_shortcode('planty_display_fraise', 'planty_fraise');
